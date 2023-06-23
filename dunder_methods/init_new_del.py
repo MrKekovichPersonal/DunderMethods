0238d772181d22ffebe8717611187ff9c107ba23
MrKekovich <stas.kuryanov50@mail.ru>
2023-06-23 15:35:44 +0300

    what are dunder methods

---
+++ dunder_methods/init_new_del.py
@@ -0,0 +1,23 @@
+"""
+__new__ is called when object is being created.
+"""
+
+class MyClass:
+    prop1 = "'I'm property'"  # class property
+    prop2 = 'hey!'  # class property
+    countdown = 5
+
+    def __init__(self, prop3, prop4):
+        self.prop1 = prop3  # instance property
+        self.prop2 = prop4  # instance property
+        self.countdown = 0  # won't affect class property, but will rewrite instance property
+
+    @classmethod
+    def class_method1(cls):  # notice the cls. It is used to access class attributes such as prop1 and prop2
+        return [cls.prop1, cls.prop2]
+
+    @classmethod
+    def class_method2(cls):  # also cls is used to access class methods
+        return ['I am class method', cls.class_method1()]
+
+
--- dunder_methods/what_are_dunder_methods.py
+++ dunder_methods/what_are_dunder_methods.py
@@ -1,18 +1,33 @@
 """
 dunder = double underscore
+python version: 3.10 99 in total
+few examples:
 __init__ ; __add__ ; __str__
 """
 
 
-class Example:
-    def __init__(self, number: int):
-        print("Calling init")
-        self.number = number
+class Point:  # let's imagine we need to calculate points in 2d dimension.
+    def __init__(self, x, y):  # point has x and y
+        print('Calling __init__')
+        self.x = x
+        self.y = y
 
-    def __add__(self, other):
-        print("Calling add")
-        return self.number + other
+    def __add__(self, other):  # when we add two points
+        print('Calling __add__')
+        # we have to calculate new point by adding x and y of both points
+        return Point(self.x + other.x, self.y + other.y)  # and return new point
 
-    def __str__(self):
-        print("Calling str")
-        return str(self.number)
+    # but oh! What do we print? X? Y?
+    def __str__(self):  # here comes the magic __str__ method
+        print('Calling __str__')
+        return f"({self.x}, {self.y})"  # we simply return two coordinates of a point
+
+
+point_1 = Point(1, 2)  # calls __init__
+point_2 = Point(3, 4)  # calls __init__
+
+print(point_1)
+
+print(point_1 + point_2)  # calls __add__  | returns new Point(4, 6) and calls __str__ for this object
+
+str(point_2)  # calls __str__
